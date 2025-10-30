2dad6c2b451894eb8eddb3d257df82dcb5371263
Franco <frfantusatti@protonmail.com>
2025-10-30 07:59:07 -0300

    fixed redirect before auth

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -10,7 +10,6 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const token_hash = searchParams.get("token_hash");
   const type = searchParams.get("type") as EmailOtpType | null;
-  const next = searchParams.get("next") ?? "/";
 
   if (token_hash && type) {
     const supabase = await createClient();
@@ -34,7 +33,7 @@ export async function GET(request: NextRequest) {
         },
       });
 
-      redirect(next);
+      redirect("/dashboard");
     } else {
       redirect(`/auth/error?error=${error?.message}`);
     }
